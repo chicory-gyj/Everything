aa277713c127797dae17954608665ab19308c755
root <root@fang.(none)>
2015-02-06 13:39:38 +0800

    [fangz] fix pandoc html to no suffix

--- pandoc.pl
+++ pandoc.pl
@@ -6,7 +6,7 @@ my ($arg, $temp, @split);
 foreach $arg (@ARGV)
 {
     @split = split /\./, $arg;
-    $temp = $split[0]."\.html";
+    $temp = $split[0];
     print $temp,"\n";
-    system "pandoc $arg -s -o $temp";
+    system "pandoc $arg -t html -s -o $temp";
 }
