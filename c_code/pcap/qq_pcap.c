340781ab510479bda5164529ecfe3e758fd3580e
fangzheng <fangzheng@gbcom.com.cn>
2015-08-12 13:16:55 +0800

    haha

--- c_code/pcap/qq_pcap.c
+++ c_code/pcap/qq_pcap.c
@@ -8,14 +8,36 @@ void __init init()
 void callback(u_char *userless, const struct pcap_pkthdr* pkthdr, const u_char* packet)
 {
     const u_char* tmp = packet;
-    if(tmp[42] == 0x02) /*qq flag must be 0x02*/
+    int len = pkthdr->len;
+    //printf("caplen:%d\n", pkthdr->caplen);
+    //printf("len:%d\n", pkthdr->len);
+    if((tmp[42] == 0x02) && (tmp[len-1] == 0x03)) /*qq flag must be 0x02*/
     {
-        printf("mac:%x:%x:%x:%x:%x:%x\n",tmp[6],tmp[7],tmp[8],tmp[9],tmp[10],tmp[11]);/*user mac*/
+        printf("udp mac:%x:%x:%x:%x:%x:%x\n",tmp[6],tmp[7],tmp[8],tmp[9],tmp[10],tmp[11]);/*user mac*/
         //printf("%x\n", tmp[42+0]); /*flag*/
         //printf("%x%x\n", tmp[42+1], tmp[42+2]);/*version*/
         //printf("%x%x\n", tmp[42+3], tmp[42+4]);/*command*/
         //printf("%x%x\n", tmp[42+5], tmp[42+6]);/*squence*/
-        printf("qq:%x%x%x%x\n",tmp[42+7], tmp[42+8], tmp[42+9], tmp[42+10]);  /*qq number*/
+        printf("udp qq:%x%x%x%x\n",tmp[42+7], tmp[42+8], tmp[42+9], tmp[42+10]);  /*qq number*/
+    }
+    else if((tmp[68] == 0x02) && (tmp[len-1] == 0x03))
+    {
+        printf("tcp mac:%x:%x:%x:%x:%x:%x\n",tmp[6],tmp[7],tmp[8],tmp[9],tmp[10],tmp[11]);/* user mac*/
+        printf("tcp qq:%x%x%x%x\n",tmp[68+7], tmp[68+8], tmp[68+9], tmp[68+10]);  /* qq number*/
+    }
+    else if((tmp[54]==0)&&(tmp[55]==0)&&(tmp[58]==0)&&(tmp[59]==0)&&(tmp[60]==0)&&(tmp[61]==0x09)&&(tmp[62]==0x01)&&(tmp[63]==0))
+    {
+        int qq_len = tmp[71] - 4;
+        printf("android qq_len:%d\n",qq_len);
+        printf("android tcp mac:%x:%x:%x:%x:%x:%x\n",tmp[6],tmp[7],tmp[8],tmp[9],tmp[10],tmp[11]);/* user mac*/
+        printf("android qq:%.*s\n", qq_len, tmp+72);
+    }
+    else if((tmp[54]==0)&&(tmp[55]==0)&&(tmp[58]==0)&&(tmp[59]==0)&&(tmp[60]==0)&&(tmp[61]==0x08)&&(tmp[62]==0x01)&&(tmp[63]==0))
+    {
+        int ios_len = tmp[135] - 4;
+        printf("ios qq_len:%d\n",ios_len);
+        printf("ios tcp mac:%x:%x:%x:%x:%x:%x\n",tmp[6],tmp[7],tmp[8],tmp[9],tmp[10],tmp[11]);/* user mac*/
+        printf("ios qq:%.*s\n", ios_len, tmp+136);
     }
 }
 
@@ -25,7 +47,7 @@ int main(int argc, char *argv[])
     char *dev;          /* The device to sniff on */
     char errbuf[PCAP_ERRBUF_SIZE];  /* Error string */
     struct bpf_program fp;      /* The compiled filter */
-    char filter_exp[] = "port 8000";  /* The filter expression */
+    char filter_exp[] = "udp dst port 8000 or tcp dst port 443 or tcp dst port 8080";  /* The filter expression */
     bpf_u_int32 mask;       /* Our netmask */
     bpf_u_int32 net;        /* Our IP */
     struct pcap_pkthdr header;  /* The header that pcap gives us */
@@ -44,7 +66,7 @@ int main(int argc, char *argv[])
         mask = 0;
     }
     /* Open the session in promiscuous mode */
-    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    handle = pcap_open_live("eth5", BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
         return(2);
