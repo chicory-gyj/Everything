bb30af1770ae32b75a921cc4b1758693f4233273
fangzheng <fangzheng@gbcom.com.cn>
2016-01-06 15:43:50 +0800

    --wip--

--- perl/stockconvert.pl
+++ perl/stockconvert.pl
@@ -1,24 +1,25 @@
 #!/bin/env perl
-$id = "sh000001";
-$filename = "/samba/";
-$filename .=$id;
-$filename .=".org";
+$filename = "/samba/sh000001.org";
 open file ,"<","$filename";
 
-$stockfile = '~/github/fabius8.github.io/stock/stock.org';
+$stockfile = '/home/fabius8/github/fabius8.github.io/stock/stock.org';
 open sfile ,">>$stockfile";
 
 while(<file>)
 {
     if($_ =~ s/\* //)
     {
+        chomp;
         $date = $_;
         print $_;
+        print sfile "| ", $_, " | ";
     }
     else
     {
+        chomp;
         $_ =~ /09:29 (\d+)亿 ((-?)(\d+).(\d+)) .* 11:30 (\d+)亿 ((-?)(\d+).(\d+)) .* 15:00 (\d+)亿 ((-?)(\d+).(\d+))/;
-        print $1," ", $2, " ", $3, " ", $4, " ", $5, " ", $6 ;
+        print $1," ", $2, " ", $6, " ", $7, " ", $11, " ", $12 ;
+        print sfile $1, " | ",$2, " | ", $6, " | ", $7, " | ", $11," | ", $12, " | ", " | |\n";
         print "\n";
     }
 }
