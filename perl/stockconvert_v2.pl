87a5a8db0dd77ef9b901cda2c39022660f6361b2
fangzheng <fangzheng@gbcom.com.cn>
2016-03-04 13:20:06 +0800

    auto update

---
+++ perl/stockconvert_v2.pl
@@ -0,0 +1,30 @@
+#!/bin/env perl
+$filename = "/samba/sz399001.org";
+open file ,"<","$filename";
+
+$stockfile = '/home/fabius8/github/fabius8.github.io/stock/stock.org';
+open sfile ,">>$stockfile";
+
+while(<file>)
+{
+    if($_ =~ s/\* //)
+    {
+        chomp;
+        $date = $_;
+        print $_;
+        print sfile "| ", $_, " | ";
+    }
+    else
+    {
+        chomp;
+        $_ =~ /09:29 (\d+)亿 ((-?)(\d+).(\d+)) .* 11:30 (\d+)亿 ((-?)(\d+).(\d+)) .* 15:00 (\d+)亿 ((-?)(\d+).(\d+))/;
+        print $1," ", $2, " ", $6, " ", $7, " ", $11, " ", $12 ;
+        print sfile $1, " | ",$2, " | ", $6, " | ", $7, " | ", $11," | ", $12, " | ", " | |\n";
+        print "\n";
+    }
+}
+
+close file;
+
+
+
