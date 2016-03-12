48245dd574ef8218ceff3a4b602aeb035aa7f55c
fangzheng <fangzheng@gbcom.com.cn>
2016-03-12 19:20:07 +0800

    auto update

---
+++ c_code/pcap/tuniu.c
@@ -0,0 +1,57 @@
+#include <pcap.h>
+#include <stdio.h>
+#define __init __attribute__ ((constructor(101)))
+void __init init()
+{
+    printf("start...\n");
+}
+void callback(u_char *userless, const struct pcap_pkthdr* pkthdr, const u_char* packet)
+{
+    const u_char* tmp = packet;
+    int len = pkthdr->len;
+    printf("caplen:%d\n", pkthdr->caplen);
+
+}
+
+int main(int argc, char *argv[])
+{
+    pcap_t *handle;         /* Session handle */
+    char *dev;          /* The device to sniff on */
+    char errbuf[PCAP_ERRBUF_SIZE];  /* Error string */
+    struct bpf_program fp;      /* The compiled filter */
+    char filter_exp[] = "tcp port 80";  /* The filter expression */
+    bpf_u_int32 mask;       /* Our netmask */
+    bpf_u_int32 net;        /* Our IP */
+    struct pcap_pkthdr header;  /* The header that pcap gives us */
+    const u_char *packet;       /* The actual packet */
+
+    /* Define the device */
+    dev = pcap_lookupdev(errbuf);
+    if (dev == NULL) {
+        fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+        return(2);
+    }
+    /* Find the properties for the device */
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+        net = 0;
+        mask = 0;
+    }
+    /* Open the session in promiscuous mode */
+    handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        return(2);
+    }
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return(2);
+    }
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return(2);
+    }
+    pcap_loop(handle, 0, callback, NULL);
+    pcap_close(handle);
+    return(0);
+}
