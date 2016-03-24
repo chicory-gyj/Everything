15b6f64f1fdb1df5474d814b32b58010f688d4f1
fangzheng <fangzheng@gbcom.com.cn>
2016-03-24 18:10:09 +0800

    auto update

--- perl/request_athenticator.pl
+++ perl/request_athenticator.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 use Digest::MD5 qw(md5 md5_hex md5_base64);
-
+###################################################
 @msg1 = (0x28, 0x02, 0x00, 0x34,
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -12,7 +12,7 @@ use Digest::MD5 qw(md5 md5_hex md5_base64);
 );
 $msg_pack1 = pack("C*", @msg1);
 $password1 = "cmedia";
-
+###################################################
 @msg2 = (0x28, 0x01, 0x00, 0x17,
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -20,15 +20,25 @@ $password1 = "cmedia";
 );
 $msg_pack2 = pack("C*", @msg2);
 $password2 = "123456";
+###################################################
+@msg3 = (0x28, 
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+);
+$msg_pack3 = pack("C*", @msg3);
+$password3 = "cmedia";
+###################################################
 
+$msg = $msg_pack3;
+$password = $password3;
 print "msg:  ";
-print unpack("H*", $msg_pack1);
+print unpack("H*", $msg);
 
 print "\n";
-print "password: ", $password1;
+print "password: ", $password;
 
-$all = $msg_pack1;
-$all .= $password1;
+$all = $msg;
+$all .= $password;
 $authenticator = md5($all);
 
 print "\n";
