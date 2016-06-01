890381429c18c6f5eca6eca697a544e9638bc93d
fangzheng <fangzheng@gbcom.com.cn>
2016-06-01 10:50:09 +0800

    auto update

--- c_code/memcpy.c
+++ c_code/memcpy.c
@@ -1,7 +1,9 @@
 int main()
 {
     unsigned char test[2] ={0};
-    memcpy(test, int*(0x1122), 2);
 
     printf("%d %d\n", test[0], test[1]);
+    int a;
+    a = 0xdeadcafe;
+    printf("%", a);
 }
