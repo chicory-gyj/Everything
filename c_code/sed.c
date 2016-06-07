2e4bdf0af31c846ad553697e8f30c8958d940359
fangzheng <fangzheng@gbcom.com.cn>
2016-06-07 10:40:08 +0800

    auto update

--- c_code/sed.c
+++ c_code/sed.c
@@ -1,12 +1,20 @@
 int main()
 {
+#define AUDIT_ZKXY_CONFIG_FILE "filter_zkxy.conf"
     char *seraddr = "11.11.11.11:9000";
     char addr[32] = {0};
     char port[32] = {0};
     char command[1024] = {0};
     sscanf(seraddr, "%[^:]:%[^:]", addr, port);
-
     printf("%s %s",addr,port);
-    //sprintf(command, "sed -i 's/parent_center_ip.*=.*/parent_center_ip=%s'", pstConf->acAuditServerAddr);
-    //system(command);
+
+    sprintf(command, "sed -i 's/parent_center_ip.*=.*/parent_center_ip=%s/' %s", addr, AUDIT_ZKXY_CONFIG_FILE);
+    system(command);
+    memset(command, 0, sizeof(command));
+    sprintf(command, "sed -i 's/parent_center_port.*=.*/parent_center_port=%s/' %s", port, AUDIT_ZKXY_CONFIG_FILE);
+    system(command);
+    memset(command, 0, sizeof(command));
+    sprintf(command, "sed -i 's/mirror_device_list.*=.*/mirror_device_list=%s/' %s", "sbssss", AUDIT_ZKXY_CONFIG_FILE);
+    system(command);
+    return 0;
 }
