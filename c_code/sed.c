e8c253a0a6b768ca3d187549a058a3452ae76446
fangzheng <fangzheng@gbcom.com.cn>
2016-06-07 10:20:08 +0800

    auto update

---
+++ c_code/sed.c
@@ -0,0 +1,12 @@
+int main()
+{
+    char *seraddr = "11.11.11.11:9000";
+    char addr[32] = {0};
+    char port[32] = {0};
+    char command[1024] = {0};
+    sscanf(seraddr, "%s:%s", addr, port);
+
+    printf("%s:%s",addr,port);
+    //sprintf(command, "sed -i 's/parent_center_ip.*=.*/parent_center_ip=%s'", pstConf->acAuditServerAddr);
+    //system(command);
+}
