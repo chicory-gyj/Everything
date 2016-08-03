b4f49c0d5dcae235b7680123ea845f1732c07001
fabius8 <fabius8@163.com>
2016-08-03 12:49:52 +0800

    --wip--

--- shell/calc_rx.sh
+++ shell/calc_rx.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+device=$1
+
+if [ "$device" ];then
+	echo calculating $device rate ...
+	echo you can use this to see: tail -f $device
+else
+	echo "Please input device!"
+	exit 1
+fi
+
 intervalo=1
 info="/sys/class/net/"
 cd $info
 while [[ 1 > 0 ]]
 do
-    for interface in eth*
+    for interface in $device
     do
         rx1=`cat $info$interface/statistics/rx_bytes`
         tx1=`cat $info$interface/statistics/tx_bytes`
         `sleep $((intervalo))s`
         rx2=`cat $info$interface/statistics/rx_bytes`
         tx2=`cat $info$interface/statistics/tx_bytes`
-        echo $interface
-        echo ----
-        echo RX: $((($rx2-$rx1)/($intervalo*1024))) Kbps
-        echo TX: $((($tx2-$tx1)/($intervalo*1024))) Kbps
+        echo $interface >> ~/$interface
+        echo ---- >> ~/$interface
+        echo RX: $((($rx2-$rx1)/($intervalo*1024))) Kbps >> ~/$interface
+        echo RX: $((($rx2-$rx1)/($intervalo*1024*1024))) Mbps >> ~/$interface
+        echo TX: $((($tx2-$tx1)/($intervalo*1024))) Kbps >> ~/$interface
+        echo TX: $((($tx2-$tx1)/($intervalo*1024*1024))) Mbps >> ~/$interface
+	echo `date` >> ~/$interface
     done
 done
