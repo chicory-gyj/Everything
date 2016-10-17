6a1c02acb5fc5759ba2fab3adee859965826d9e3
fabius8 <fabius8@163.com>
2016-10-17 09:27:41 +0800

    --wip--

--- node.js/Ex8.js
+++ node.js/Ex8.js
@@ -1,6 +1,13 @@
 var http = require('http')
+var bl = require('bl')
 
 http.get(process.argv[2], function (response) {
-    response.setEncoding('utf8')
-    response.on('data', console.log)
+    response.pipe(bl(function (err, data) {
+        if (err) {
+            return console.error(err)
+        }
+        data = data.toString()
+        console.log(data.length)
+        console.log(data)
+    }))
 })
